70ae3f9ab04a48d22d8a61679d44ea77435e82df
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2023-01-13 15:01:30 +0300

    created an registration form used in auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,37 @@
 import bcrypt from  "brycpt";
 import { Jwt } from "jsonwebtoken";
 import user from "../models/User.js";
+
+/*REGISTER USER */
+
+export const register = async (req, res) => { /* the async creates a connection between mongodb backend and the frontend when user request for data */
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation,
+        } = req.body; /*request body*/
+         
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User ({
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation,
+
+        })
+    } catch (err){
+
+    }
+}
