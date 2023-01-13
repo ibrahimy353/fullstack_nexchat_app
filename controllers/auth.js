b7962060331201bcdad0901704fb39f7c085c9fe
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2023-01-13 16:22:44 +0300

    created a login set up

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ import { json } from "body-parser";
 import bcrypt from  "bcrypt";
 import { Jwt } from "jsonwebtoken";
 import user from "../models/User.js";
+import User from "../models/User.js";
 
 /*REGISTER USER */
 
@@ -43,8 +44,28 @@ export const register = async (req, res) => { /* the async creates a connection 
          backend to provide the user the correct details if the authentication provied is correct or doesn't error out */
     } catch (err){
         res.status(500).json({error: err.message});
-        //in casses of error or wrong auth this what the user will get in the frontend
+        //in casses of error or wrong auth this what the user will get in the frontend0
+                }
+};
+
+/*LOGGING IN*/
+
+export const login = async ( req, res) =>{
+    try {
+        const { email, password } = req.body;// this grabs the email and password when the usr tries to login
+        const user = await User.findOne({ email: email});//this here uses mongose to try and bring back the correct data req of the user once they tr and login
         
+        if (!user) return res.status(400).json({msg: "User does not Exist."});
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) return res.status(400).json({msg: "Invalid credentials"});
+
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({token, user});
 
     }
+    catch (err){
+        res.status(500).json({error: err.message});
+}
 };
