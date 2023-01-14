066d6eb2a7ced8ff1c27b1d85d63ee6af0d184d1
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2023-01-14 18:10:24 +0300

    updated the friends add and remove function in update section

--- controllers/user.js
+++ controllers/user.js
@@ -43,9 +43,14 @@ export const addRemoveFriend = async( req, res) =>{
         const friend = await User.findById(friendId);
 
         if (user.friends.includes(friendId)) {
+            //if the friend id is part of the main friends user id list we remove them through using the line bellow.
+            //the filter function removes the friends id if the user id is equal as the user
             user.friends = user.friends.filter((id) => id !== friendId);
+            friend.friends = friends.friends.filter((id) => id !== id);
         }
-    }
+    }  catch(err){
+    res.status(404).json({message: err.message})
+}
 } 
 
 
