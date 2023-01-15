961b85c5cdccdc5ebbfd8f7d3c91c6dfe980d57d
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2023-01-15 16:46:15 +0300

    commented out the insertmany function

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ import User from "./models/User.js";
 import Post from "./models/Post.js";
 import {users } from "./data/index.js"
 import { posts } from "./data/index.js";
+//import { users, posts} from "./data/index.js" is the same as just the last two import on top of this line
 /* Configuration */
 
 const __filename = fileURLToPath(import.meta.url);
@@ -66,9 +67,9 @@ mongoose
 .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
       
- /*ADD THE DATA ONE TIME*/
-  user.insertMany(users);
-    //Post.insertMany(posts);
+ /*ADD DATA ONE TIME*/
+  //User.insertMany(users);
+   // Post.insertMany(posts);
 })
 
 .catch((error) => console.log(`${error} did not connect`));
