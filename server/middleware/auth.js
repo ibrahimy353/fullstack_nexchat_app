16627cd23614c7d182e5bda946a3b55b848e0f5e
ibrahim yusuf <85551204+ibrahimy353@users.noreply.github.com>
2023-01-16 16:32:39 +0300

    correction

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,14 +1,14 @@
 import jwt  from "jsonwebtoken";
 // the middleware here verifies the api end points in which it 
-//allow the authorized user to be able to do things in which the non loged in user cant access or do
+//allow the authorized user to be able to do things in which the non logged in user cant access or do
 
 export const verifyToken = async (req, res, next) =>{ 
     try{
         let token = req.header("Authorization");
-//if the token entered doesnt exist then the system will give back output Access denaid
+//if the token entered doesnt exist then the system will give back output Access denied
 //this is done through grabbing token from the front end to back end
         if(!token ){
-            return res.status(403).send("Access Denaid");
+            return res.status(403).send("Access Denied");
         }
 
         if (token.startswith("Bearer ")) {
